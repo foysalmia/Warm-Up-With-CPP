fca7891036a54fffd619e12c031498ebe0cfece5
foysalmia <mfoysal314@gmail.com>
2022-07-15 00:52:08 +0600

    E done

---
+++ E/E.cpp
@@ -0,0 +1,37 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int x = 0,n;
+    cin>>n;
+    int arr[n];
+    char s;
+    for(int i=0;i<n;i++){
+        cin>>s;
+        if(s == 'I'){
+            x++;
+            arr[i] = x;
+        }else{
+            x--;
+            arr[i] = x;
+        }
+    }
+
+    int m = arr[0];
+    for(int i=0;i<n;i++){
+        if(arr[i]>m)
+            m = arr[i];
+    }
+
+    if(m<0){
+        m=0;
+    }
+
+    cout<<m<<endl;
+
+}
+
+
