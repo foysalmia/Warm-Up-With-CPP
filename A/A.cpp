a82ae5f5dc2acd8c962d28adc336bae5e472e9b0
Foysal Mia <76910540+foysalmia@users.noreply.github.com>
2022-07-16 14:24:46 +0600

    Updated A and done with submit

--- A/A.cpp
+++ A/A.cpp
@@ -1,38 +1,20 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 
-int main()
-{
-    long long a, b, x,counter = 0;
-    cin >> a >> b >> x;
-    while(a>0 && b>0){
-        a--;
-        b--;
-        counter++;
-    };
-     while(b>0 && x>0){
-        b--;
-        x--;
-        counter++;
-    };
-     while(a>0 && x>0){
-        a--;
-        x--;
-        counter++;
-    }
-     while(a>0 && b>0 && x>0){
-        a--;
-        b--;
-        x--;
-        counter++;
-    }
+int main() {
+  long long int arr[3];
 
-    if(a==0 && b==0 && x==0){
-        cout<<counter<<endl;
-    }else{
-        cout<<"-1"<<endl;
-    }
-    return 0;
+  for (int i = 0; i < 3; i++) {
+    cin >> arr[i];
+  }
+
+  sort(arr, arr + 3);
+
+  if (arr[2] - arr[1] <= arr[0]) {
+    cout << arr[2] << endl;
+  } else {
+    cout << -1 << endl;
+  }
+
+  return 0;
 }
-
